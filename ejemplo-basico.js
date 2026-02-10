fddfc980488f6e35ac0289fa3f6d930e46850e46
Brayan Leal <balealr@correo.url.edu.gt>
2026-02-09 18:24:07 -0600

    Parte A

---
+++ ejemplo-basico.js
@@ -0,0 +1,13 @@
+﻿// Ejemplo básico de JavaScript
+const nombre = "Ana";
+const edad = 25;
+
+function saludar(nombre, edad) {
+  return `Hola, ${nombre}. Tienes ${edad} años.`;
+}
+
+console.log(saludar(nombre, edad));
+
+// Operaciones simples
+const suma = 5 + 3;
+console.log("5 + 3 =", suma);
