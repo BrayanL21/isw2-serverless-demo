f9fb26d61ebf5eaced8cbddf1c175091c7b6fe65
Brayan Leal <balealr@correo.url.edu.gt>
2026-02-11 22:23:44 -0600

    RETO 5

--- api/procesar.js
+++ api/procesar.js
@@ -6,17 +6,32 @@
 //   });
 // }
 
+// export default function handler(req, res) {
+//   const nombre = req.query.nombre || "anónimo";
+
+//   res.status(200).json({
+//     resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
+//     longitud: nombre.length
+//   });
+// // res.status(200).json({
+// //   resultado: ""
+// // });
+
+
+// }
+
 export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
 
+  // Simulación de falla controlada
+  if (nombre === "error") {
+    return res.status(500).json({
+      mensaje: "Error simulado en el sistema"
+    });
+  }
+
   res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
-    longitud: nombre.length
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
   });
-// res.status(200).json({
-//   resultado: ""
-// });
-
-
 }
 
